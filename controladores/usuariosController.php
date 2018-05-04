62be53e0db9b1634303edaa7a9c2d9bbf1377f78
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-04 09:41:06 -0500

    B: Creacion de registro de usuarios controller fase 2

--- controladores/usuariosController.php
+++ controladores/usuariosController.php
@@ -6,8 +6,23 @@ class UsuariosController
 {
 	public function registrarUsuarioController()
 	{
-		if (isset($_POST["nombreUsuario"])) {
-			# code...
+		if (isset($_POST["nombre"])) 
+		{
+			$datos = [
+				"nombre" => $_POST["nombre"],
+				"apellidos" => $_POST["apellidos"],
+				"cedula" => $_POST["cedula"],
+				"telefono" => $_POST["telefono"],
+				"correo" => $_POST["correo"],
+				"direccion" => $_POST["direccion"],
+				"cargo" => $_POST["cargo"],
+				"usuario" => $_POST["usuario"],
+				"password" => $_POST["password"],
+				"password2" => $_POST["password2"],
+				"estado" => $_POST["estado"]
+			];
+
+			$response = usuariosModel::registrarUsuariosModel($datos,"usuarios");
 		}
 	}
 }
