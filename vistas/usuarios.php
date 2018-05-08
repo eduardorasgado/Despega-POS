76f490ed13be277b22958ef653c63161e93b6476
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-08 12:42:57 -0500

    F. MOdificacion de usaurios.php para agregar tabla

--- vistas/usuarios.php
+++ vistas/usuarios.php
@@ -12,14 +12,34 @@
               <div class="col-md-12">
                   <div class="box">
                     <div class="box-header with-border">
-                          <h1 class="box-title">Tabla <button class="btn btn-success" id="btnagregar"><i class="fa fa-plus-circle"></i> Agregar</button></h1>
+                          <h1 class="box-title">
+                            <button class="btn btn-primary btn-lg" id="add_button" data-toggle="modal" data-target="#usuarioModal"><i class="fa fa-plus-plus" aria-hidden="true"></i> Nuevo Usuario</button></h1>
                         <div class="box-tools pull-right">
                         </div>
                     </div>
                     <!-- /.box-header -->
                     <!-- centro -->
-                    <div class="panel-body table-responsive" id="listadoregistros">
-                       
+                    <div class="panel-body table-responsive">
+                       <table id="usuario_data" class="table table-bordered table-striped">
+                         <thead>
+                           <tr>
+                             <th>Nombre</th>
+                             <th>Apellido</th>
+                             <th>Cédula</th>
+                             <th>Teléfono</th>
+                             <th>Correo</th>
+                             <th>Dirección</th>
+                             <th>Cargo</th>
+                             <th>Usuario</th>
+                             <th>Estado</th>
+                             <th width="10%">Editar</th>
+                             <th width="10%">Eliminar</th>
+                           </tr>
+                         </thead>
+                         <tbody>
+                           
+                         </tbody>
+                       </table>
                     </div>
                   
                     <!--Fin centro -->
