cfdce42d89ec8a260809edc8ac07a60dd915e4aa
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-08 02:21:02 -0500

    F: Se termina listar con datatables y ajax en usuarios.js de las views

--- vistas/js/usuarios.js
+++ vistas/js/usuarios.js
@@ -30,7 +30,7 @@ function limpiarCampos()
 function listar()
 {
 	//libreria para hacer tablas rapidas y funcionales
-	table = $("usuario_data").dataTable({
+	table = $("#usuario_data").dataTable({
 		"aProcessing": true, //Activamos el procesamiento del datatables
 		"aServerSide": true, //Paginacion y filtrado realizados por el servidor
 		dom: 'Bfrtip', //Definimos los elementos del control de tabla
@@ -50,6 +50,36 @@ function listar()
 				console.log(e.responseText);
 			}
 		},
+		"bDestroy": true,
+		"responsive": true,
+		"bInfo": true, //info de los datatable
+		"iDisplayLength": 10, //Por cada 10 registros hace una paginacion
+		"order": [[0,"desc"]], //ordena (columna, orden)
+
+		"language": {
+			"sProcessing": "Procesando...",
+			"sLengthMenu": "Mostrar _MENU_ registros",
+			"sZeroRecords": "No se encontraron resultados",
+			"sEmptyTable": "Ningún dato disponible en esta tabla",
+			"sInfo": "Mostrando un total de _TOTAL_ registros",
+			"sInfoEmpty": "Moatrando un total de 0 registros",
+			"sInfoFiltered": "(filtrado un total de _MAX_ registros)",
+			"sInfoPostFix": "",
+			"sSearch": "Buscar: ",
+			"sUrl": "",
+			"sInfoThousands": ",",
+			"sLoadingRecords": "Cargando...",
+			"oPaginate": {
+				"sFirst": "Primero",
+				"sLast": "Último",
+				"sNext": "Siguiente",
+				"sPrevious": "Anterior"
+			},
+			"oAria": {
+				"sSortAscending": "Activar para ordenar ascendentemente",
+				"sSortDescending": "Activar para ordenar descendentemente"
+			}
+		} //cierre de lenguaje
 	}).DataTable();
 }
 
